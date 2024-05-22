bebbb19566cd6301adb2ef1235e67a0eea29588a
cperdomo12cr <cperdomo@arcetec.com.co>
2024-05-22 16:23:31 -0500

    :sparkles: feat: creando servicio y controlador para los turnos

---
+++ app/Services/TurnService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+class TurnService
+{
+
+    public function createTurn(Request $request)
+    {
+        return 'Turn created';
+    }
+
+}
