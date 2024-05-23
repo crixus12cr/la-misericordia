3adbc365c2a4270e3fafce1d4819b9f04811cc69
cperdomo12cr <cperdomo@arcetec.com.co>
2024-05-23 08:19:33 -0500

    :art: feat: nuevas funcionalidades seeders y admin en la vista

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
        $this->call([
         TypeDocumentSeeder::class,
         CategorySeeder::class,
+        UserSeeder::class,
     ]);
     }
 }
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,11 +14,11 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="font-sans text-gray-900 antialiased" style="background-color: #0c4f66;">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="https://i.pinimg.com/564x/bc/23/8e/bc238e5e228639feb7fc9eb676584beb.jpg" class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
